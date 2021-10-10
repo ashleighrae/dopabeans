23a5b56130d94b29438d75441cf29ba8d2ecc88e
ashleighrae <67845170+ashleighrae@users.noreply.github.com>
2021-10-10 16:22:08 +1000

    Fixed up page layout for popup.

--- development/popup.js
+++ development/popup.js
@@ -1,7 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    var button = document.getElementById("submit")
-   
-    button.addEventListener("click", (e) => {
-      console.log(e)
-    })
+
+  /* Add href link to input */
+  document.getElementById('copy-link').value = window.location.href;
+
+  /* Copy link to clipboard */
+  var button = document.getElementById("copy-link")
+
+  button.addEventListener("click", (e) => {
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(window.location.href);
+
+    /* Alert the copied text */
+    alert("Copied the text: " + window.location.href);
+  })
+
 })
