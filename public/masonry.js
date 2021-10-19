1e8c38db644d776bbd1d191524317c448bd0a8a6
cassandra17 <cassandra.carse@gmail.com>
2021-10-20 02:37:05 +1000

    button on home page

--- public/masonry.js
+++ public/masonry.js
@@ -1,47 +1,19 @@
 // external js: masonry.pkgd.js
-// var $grid = $('.grid').masonry({
-//   // disable initial layout
-//   initLayout: false,
-//   //...
-// });
-
-// var $grid = $('.grid').imagesLoaded( function() {
-//   // init Masonry after all images have loaded
-//   $grid.masonry({
-//     // options...
-//     initLayout: false,
-//   });
-// });
-
-
 var $grid = $('.grid').masonry({
-  // options...
+  // disable initial layout
   initLayout: false,
-  // percentPosition: true,
-  itemSelector: '.grid-item',
-  columnWidth:  '.grid-sizer',
-  gutter: 10
+  //...
 });
-// layout Masonry after each image loads
-
-window.addEventListener("DOMContentLoaded", () => {
-  $grid.imagesLoaded().progress( function() {
-    $grid.masonry();
-  });
-})
-
-
 // bind event
-// $grid.masonry( 'on', 'layoutComplete', function() {
-//   console.log('layout is complete');
-// });
-
-// // $('.grid').masonry({
-  
-// // });
+$grid.masonry( 'on', 'layoutComplete', function() {
+  console.log('layout is complete');
+});
+// trigger initial layout
+$grid.masonry();
 
 
-// // trigger initial layout
-// $grid.masonry();
-
-
+$('.grid').masonry({
+    itemSelector: '.grid-item',
+    columnWidth:  '.grid-sizer',
+    gutter: 10
+  });
